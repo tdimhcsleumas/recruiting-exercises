a07ae27ceb55208095b215ce94ba56ac9605095c
Samuel Schmidt <tdimhcsleumas@gmail.com>
2020-08-24 22:41:10 -0700

    documentation and cleanup

--- inventory-allocator/src/inventory_allocator.py
+++ inventory-allocator/src/inventory_allocator.py
@@ -1,27 +1,45 @@
 # inventory-allocator.py
 # Samuel Schmidt 2020-08-24
 
+""" helper class to make querying items more convenient
+    these items will be store in a dictionary, keyed in by name
+    ex:
+    "chicken" : {
+        "total": 10,
+        "warehouse: {
+            "od": 8,
+            "om": 2
+        }
+    }
+
+"""
 class InventoryItem(dict):
     
     kTotal = "total"
     kWarehouse = "warehouse"
 
     def __init__(self):
-        self[self.kTotal] = 0
-        self[self.kWarehouse] = dict()
+        self["total"] = 0
+        self["warehouse"] = dict()
 
-
+    """ increment the total number of this item. generate the name, quantity pair
+        for the specified warehouse
+    """
     def addWarehouse(self, name: str, quantity: int):
-        self[self.kTotal] += quantity
-        self[self.kWarehouse][name] = quantity
+        self["total"] += quantity
+        self["warehouse"][name] = quantity
 
-    def getTotal(self):
-        return self.get(self.kTotal)
+    def getTotal(self) -> int:
+        return self.get("total")
 
-    def getWarehouses(self):
-        return self.get(self.kWarehouse)
+    def getWarehouses(self) -> dict:
+        return self.get("warehouse")
 
+""" helper class to make generating the required list more convenient
+"""
 class WarehouseOrder(dict):
+    """ insert item, amount order into specified warehouse. create dict if None
+    """
     def addOrder(self, warehouse: str, item: str, amount: int):
         if self.get(warehouse) is None:
             self[warehouse] = dict()
@@ -35,9 +53,11 @@ class InventoryAllocator():
     def __init__(self, order: dict, warehouses: list):
         self.order = order
         self.warehouses = warehouses
-        self.warehouseOrders = WarehouseOrder()
         self.items = dict()
 
+    """ fill in item dictionary to survey the total number of items as well
+        as which warehouses have them.
+    """
     def generateItems(self):
         for warehouse in self.warehouses:
             name = warehouse.get("name")
@@ -48,12 +68,16 @@ class InventoryAllocator():
                 
                 self.items.get(item).addWarehouse(name, quantity)
         
+    """ After the dictionary of items is generated, surveying the complete stock,
+        process the orders. Store the order for each warehouse as a dictionary 
+        keyed in by name so that updates are efficient. Export as a list upon return.
+    """
     def process(self) -> list:
         error = None
         shipment = WarehouseOrder()
 
         for item, quantity in self.order.items():
-            if self.items.get(item).getTotal() < quantity:
+            if self.items.get(item) is None or self.items.get(item).getTotal() < quantity:
                 error = "not enough stock for {}".format(item)
                 break;
             
